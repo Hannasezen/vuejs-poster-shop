a28f2fd776223b7d331c16930f397ed50e3bb515
Hannasezen <andreeva0649@gmail.com>
2018-11-10 16:31:46 +0200

    add products and cart

--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
-console.log(Vue);
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [
+            { title: 'Item1', price: 9.99 },
+            { title: 'Item2', price: 7.99 },
+            { title: 'Item3', price: 6.99 }
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function (index) {
+            this.cart.push(this.items[index]);
+            this.total += this.items[index].price;
+        }
+    }
+});
