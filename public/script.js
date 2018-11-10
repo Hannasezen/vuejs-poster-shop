78007042d6e8041f16791e415c1a320d4edf1e91
Hannasezen <andreeva0649@gmail.com>
2018-11-10 19:23:13 +0200

    products requare

--- public/script.js
+++ public/script.js
@@ -1,18 +1,20 @@
+const PRICE = 9.99;
+
 new Vue({
     el: '#app',
     data: {
         total: 0,
-        items: [
-            { id: 1, title: 'Item1', price: 9.99 },
-            { id: 2, title: 'Item2', price: 7.99 },
-            { id: 3, title: 'Item3', price: 6.99 }
-        ],
+        items: [],
         cart: [],
         search: ''
     },
     methods: {
         onSubmit: function () {
-            console.log(this.search);
+            this.$http
+            .get('/search/'.concat(this.search))
+            .then(function(res) {
+                this.items = res.data;
+            });
         },
         addItem: function (index) {
             let item = this.items[index];
@@ -29,19 +31,19 @@ new Vue({
                     id: item.id,
                     title: item.title,
                     qty: 1,
-                    price: item.price
+                    price: PRICE
                 });
             }
             
-            this.total += item.price;
+            this.total += PRICE;
         },
         inc: function(item) {
             item.qty++;
-            this.total += item.price;
+            this.total += PRICE;
         },
         dec: function(item) {
             item.qty--;
-            this.total -= item.price;
+            this.total -= PRICE;
             if (item.qty <= 0) {
                 for(let i = 0; i < this.cart.length; i++) {
                     if (this.cart[i].id === item.id) {
