8f3273ceb0a23d6c2bfe1e4c107b4ce280d11fc5
Hannasezen <andreeva0649@gmail.com>
2018-11-10 18:11:42 +0200

    cart items buttons done

--- public/script.js
+++ public/script.js
@@ -17,6 +17,7 @@ new Vue({
                 if(this.cart[i].id === item.id) {
                     found = true;
                     this.cart[i].qty++;
+                    break;
                 }
             }
             if (!found) {
@@ -29,6 +30,27 @@ new Vue({
             }
             
             this.total += item.price;
+        },
+        inc: function(item) {
+            item.qty++;
+            this.total += item.price;
+        },
+        dec: function(item) {
+            item.qty--;
+            this.total -= item.price;
+            if (item.qty <= 0) {
+                for(let i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function(price) {
+            return '$'.concat(price.toFixed(2));
         }
     }
 });
