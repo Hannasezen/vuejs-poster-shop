8c2b2d25fa2ad1ba60f30ee825885033c8b2a482
Hannasezen <andreeva0649@gmail.com>
2018-11-10 17:08:38 +0200

    complit the addItem method

--- public/script.js
+++ public/script.js
@@ -3,16 +3,32 @@ new Vue({
     data: {
         total: 0,
         items: [
-            { title: 'Item1', price: 9.99 },
-            { title: 'Item2', price: 7.99 },
-            { title: 'Item3', price: 6.99 }
+            { id: 1, title: 'Item1', price: 9.99 },
+            { id: 2, title: 'Item2', price: 7.99 },
+            { id: 3, title: 'Item3', price: 6.99 }
         ],
         cart: []
     },
     methods: {
         addItem: function (index) {
-            this.cart.push(this.items[index]);
-            this.total += this.items[index].price;
+            let item = this.items[index];
+            let found = false;
+            for(let i = 0; i < this.cart.length; i++) {
+                if(this.cart[i].id === item.id) {
+                    found = true;
+                    this.cart[i].qty++;
+                }
+            }
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: item.price
+                });
+            }
+            
+            this.total += item.price;
         }
     }
 });
